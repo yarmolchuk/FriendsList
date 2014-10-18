8a8eb2416ff601bfde29c6a05e76fa00ab6d2711
Dima <yarmolchuk@gmail.com>
2014-10-18 12:54:32 +0300

    create project

---
+++ FriendsList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendsList
+//
+//  Created by Yarmolchuk Dima on 18.10.14.
+//  Copyright (c) 2014 Yarmolchuk Dima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
