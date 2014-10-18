e4e4435e934110586377f971e68b2b890d4f45bd
Dima <yarmolchuk@gmail.com>
2014-10-18 13:33:28 +0300

    add table view + action add friend

--- FriendsList/ViewController.swift
+++ FriendsList/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var friendsTableView: UITableView!
+    @IBOutlet weak var addFriendBtn: UIBarButtonItem!
+    
+    var friends = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "\"The List Friends\""
+        
+        friendsTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+            
+        cell.textLabel!.text = friends[indexPath.row]
+            
+        return cell
+    }
+    
+    // MARK: Memory
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: Action
+    @IBAction func actAddFriend(sender: AnyObject) {
+        
+        var alert = UIAlertController(title: "New friend", message: "Add a new friend", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default) {
+            (action: UIAlertAction!) -> Void in
+                
+            let textField = alert.textFields![0] as UITextField
+            
+            self.friends.append(textField.text)
+            self.friendsTableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default) {
+            (action: UIAlertAction!) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField!) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
